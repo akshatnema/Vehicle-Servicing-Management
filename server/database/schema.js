66acfe19ab3a023a343ad11be77cd0234f55e2b2
AKSHATNEMA <20bcs022@iiitdmj.ac.in>
2021-09-30 11:31:35 +0530

    made job_card and details

--- server/database/schema.js
+++ server/database/schema.js
@@ -9,9 +9,22 @@ con.connect(function(err) {
       });
   });
   
-var queries ="CREATE TABLE IF NOT EXISTS customer (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255),email VARCHAR(255),password VARCHAR(255), street VARCHAR(255), city VARCHAR(255), state VARCHAR(255))"  
+var customer ="CREATE TABLE IF NOT EXISTS customer (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255),email VARCHAR(255),password VARCHAR(255), street VARCHAR(255), city VARCHAR(255), state VARCHAR(255))"
 
-con.query(queries, function (err) {
+var job_card="CREATE TABLE IF NOT EXISTS job_card(card_id INT AUTO_INCREMENT PRIMARY KEY NOT NULL, customer_id INT NOT NULL,street VARCHAR(30) NOT NULL, city VARCHAR(30) NOT NULL, state VARCHAR(30) NOT NULL,job_id INT NOT NULL,chassis_no VARCHAR(15) NOT NULL, purchase_date DATE NOT NULL, stage1 BOOLEAN,stage2 BOOLEAN,stage3 BOOLEAN,license_no INT NOT NULL)"  
+
+var job_card_details="CREATE TABLE IF NOT EXISTS job_card_details(srno INT AUTO_INCREMENT PRIMARY KEY,card_id INT NOT NULL,job_id INT NOT NULL,price INT,Assign_dt DATE NOT NULL,Complete_dt DATE NOT NULL,Status BOOLEAN NOT NULL,Employee_id INT)"
+
+con.query(customer, function (err) {
   if (err) throw err;
-  console.log("Table created");
+  console.log(" Customer Table created");
+});
+
+con.query(job_card, function (err) {
+  if (err) throw err;
+  console.log("Job Card Table created");
+});
+con.query(job_card_details, function (err) {
+  if (err) throw err;
+  console.log("Job Card Details Table created");
 });
