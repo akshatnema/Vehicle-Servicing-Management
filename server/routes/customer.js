adbfc090f13b085b219d770826aed8b9483790c4
AKSHATNEMA <20bcs022@iiitdmj.ac.in>
2021-10-17 15:03:16 +0530

    updated view

--- server/routes/customer.js
+++ server/routes/customer.js
@@ -115,7 +115,7 @@ router.post('/register',async function(req, res) {
         res.redirect('/')
     }
     else{
-        const hash = await bcrypt.hash(password, 12)
+        const hash = await bcrypt.hash(password, 5)
         // const query=`INSERT INTO customer (id,name,email,mobile,street,city,state,password) VALUES ('${id}','${name}','${email}','${mobile}','${address}','${city}','${state}','${hash}')`
 
         const query ="INSERT INTO customer (name,email,street,city,state,password) VALUES (?,?,?,?,?,?)"
